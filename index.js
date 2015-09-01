31f1ce8c2fd5c1b113acd5c4e71fb66490fa63bd
Anton Yatsenko <anton.yacenko@gmail.com>
2015-09-01 16:48:17 +0300

    comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var path = './vendor/jsPDF/';
 jsPDF = require(path+'jspdf');
 
-// //Load all the plugins
+//Load all the plugins
 var plugins = ['addhtml', 'addimage', 'annotations', 'autoprint', 'cell', 'context2d', 'from_html', 'javascript', 'outline', 'png_support', 'split_text_to_size', 'standard_fonts_metrics', 'svg', 'total_pages'];
 plugins.map(function(plugin){
     require(path+'/plugins/'+plugin+'.js');
 });
 
-// //Modify the save function to save to disk
+//Modify the save function to save to disk
 var fs = require('fs');
 jsPDF.API.save = function(filename, callback){
     fs.writeFile(filename, this.output(), callback);
