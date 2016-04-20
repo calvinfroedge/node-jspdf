986f18f963f3605c503b340dc36d2e8ee21c4469
Steve Potter <spotter@arteris.com>
2016-04-19 17:28:31 -0700

    Fetch a fixed version of jspdf, instead of always getting the master (which may be incompatible). The version should be updated when a new jspdf version is qualified.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 if [ ! -d vendor/jsPDF ]; then
-    wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/master.zip
+    wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/v1.2.61.zip
     unzip jsPDF.zip
     mkdir vendor
     mv jsPDF-master vendor/jsPDF
