f4676ced604295a0cea37be89e94af675fdf89d4
Steve Potter <spotter@arteris.com>
2016-04-19 17:37:46 -0700

    Move whatever jspdf version was unzipped to vendor.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ if [ ! -d vendor/jsPDF ]; then
     wget --no-check-certificate -O jsPDF.zip https://github.com/MrRio/jsPDF/archive/v1.2.61.zip
     unzip jsPDF.zip
     mkdir vendor
-    mv jsPDF-master vendor/jsPDF
+    mv jsPDF-* vendor/jsPDF
     rm jsPDF.zip
 fi
